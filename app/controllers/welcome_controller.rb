25be9167a1860b643eab0eb3f15f2351de6dc882
Jade Kazoo <jadekzn@gmail.com>
2017-11-27 10:02:40 -0500

    did a bunch of api-related shenanigans

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def text
+  	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/OH/Columbus.json")
+    @current_observation = response['current_observation']
   end
 
   def index
