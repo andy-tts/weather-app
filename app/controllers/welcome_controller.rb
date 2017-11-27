318db26aacf4e7690aa8dd38bfe44a92e3d01de6
Jill R. Hayes <dvmjill@gmail.com>
2017-11-27 09:55:05 -0500

    display data from the api wunderground

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
   def text
+  	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/OH/Columbus.json")
+  	@location = response['current_observation']['display_location']['full']
+  	@temperature = response['current_observation']['temperature_string']
   end
 
   def index
