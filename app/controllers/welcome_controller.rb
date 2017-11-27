93b06ac0f4eaa7de0f44da6026bc46917c0e8503
Jade Kazoo <jadekzn@gmail.com>
2017-11-27 12:04:06 -0500

    Fully functional weather data display and location keeper

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,5 +5,29 @@ class WelcomeController < ApplicationController
   end
 
   def index
+  	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
+  	# @states = %w(NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ NJ).sort!
+  	if params[:city].present? && params[:state].present?
+  		city_param = URI.encode(params[:city])
+  		results = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/#{params[:state]}/#{city_param}.json")
+  		puts results
+  		if results['response']['error'].present?
+  			@error = results['response']['error']['description']
+  		else
+	  		locations_test = Location.all
+	  		caught = false
+	  		locations_test.each do |location|
+	  			if location.city == params[:city] && location.state == params[:state]
+	  				caught = true
+	  			end
+	  		end
+	  		if caught == false
+	  			city_state = Location.create(city: params[:city], state: params[:state]) 
+	  		end
+  			@current_observation = results['current_observation']
+  		end
+  	end
+  	@locations = Location.all
   end
+
 end
