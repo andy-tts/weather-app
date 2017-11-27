63acde30c370dbf6b6ac910da94e4a671209f1d4
Jill R. Hayes <dvmjill@gmail.com>
2017-11-27 14:52:25 -0500

    added migration of model Location as well as updated gem files

--- Gemfile
+++ Gemfile
@@ -40,6 +40,9 @@ gem 'figaro'
 
 gem 'jquery-rails'
 
+gem 'wdm', '>= 0.1.0' if Gem.win_platform?
+
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
+  
   def text
   	response = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/OH/Columbus.json")
   	# @location = response['current_observation']['display_location']['full']
@@ -9,16 +10,32 @@ class WelcomeController < ApplicationController
   def index
   	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
   		
-  		if params[:city].present? && params[:state].present?
-  			city_param = URI.encode(params[:city])
-  		results = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/#{params[:state]}/#{city_param}.json")
-  		if results['response']['error'].present?
-  			@error = results['response']['error']['description']
-  		else
-  			@current_observation = results['current_observation']
+  	@locations = Location.all
+
+		location_exists = false
+			if params[:city].present? && params[:state].present?
+  			Location.all.each do |location|		
+  				if location.city == params[:city] && location.state == params[:state] 
+						location_exists = true 
+  				end
+  		end
+
+			if location_exists == false 
+  				Location.create(
+  					city: params[:city],
+  					state: params[:state]
+  						)
   		end
 
   			
-  		end  		
+  	city_param = URI.encode(params[:city])
+  	results = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/#{params[:state]}/#{city_param}.json")
+  		if results['response']['error'].present?
+  			@error = results['response']['error']['description']
+
+  		else
+  			@current_observation = results['current_observation']
+  		end
+  	end	
   end
 end
