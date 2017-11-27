88eb658ace874ab349c19d5de9976ef48ae4d981
Jill R. Hayes <dvmjill@gmail.com>
2017-11-27 11:09:13 -0500

    display error codes and include encode to allow spaces in city names

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
+  get 'index' => 'welcome#index'
+
+  post 'index' => 'welcome#index'
+
+  root 'welcome#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
