3c2735cd8e88c2186d8f8df86858c62a84ba6f79
Jill R. Hayes <dvmjill@gmail.com>
2017-11-28 11:43:01 -0500

    added ability to see My Cities on map

---
+++ app/controllers/location_controller.rb
@@ -0,0 +1,7 @@
+class LocationController < ApplicationController
+  
+  def show
+  		@location = Location.find(params[:id])
+  end
+
+end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,40 +8,74 @@ class WelcomeController < ApplicationController
   end
 
   def index
-  	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
-  		
-  	@locations = Location.all
+    @states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
 
-		location_exists = false
-			if params[:city].present? && params[:state].present?
-  			Location.all.each do |location|		
-  				if location.city == params[:city] && location.state == params[:state] 
-						location_exists = true 
-  				end
-  			end
-  		# end
+    if params[:city].present? && params[:state].present?
+      city_param = URI.encode(params[:city])
+      results = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/#{params[:state]}/#{city_param}.json")
 
-			if location_exists == false 
-  				Location.create(
-  					city: params[:city],
-  					state: params[:state],
-  					latitude: latitude,
-  					longitude: longitude
-  						)
-  		end
-
-  			
-  	city_param = URI.encode(params[:city])
-
-  	results = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/#{params[:state]}/#{city_param}.json")
-  		if results['response']['error'].present?
-  			@error = results['response']['error']['description']
-
-  		else
-  			@current_observation = results['current_observation']
-  			latitude = @current_observation['display_location']['latitude']
-  			longitude = @current_observation['display_location']['longitude']
-  		end
-  		end
-  	end	
+      
+      if results['response']['error'].present?
+        @error = results['response']['error']['description']
+      else
+        @current_observation = results['current_observation']
+        latitude = @current_observation['display_location']['latitude']
+        longitude = @current_observation['display_location']['longitude']
+        create_location(params[:city], params[:state], latitude, longitude)
+      end
+    end
+    @locations = Location.all
   end
+
+  def create_location(city_param, state_param, latitude, longitude)
+    location_exists = false
+
+    Location.all.each do |location| 
+      if location.city == city_param && location.state == state_param
+        location_exists = true
+      end
+    end
+    if location_exists == false
+      Location.create(city: city_param, state: state_param, latitude: latitude, longitude: longitude)
+    end
+  end
+
+
+
+  # def index
+  # 	@states = %w(HI AK CA OR WA ID UT NV AZ NM CO WY MT ND SD NB KS OK TX LA AR MO IA MN WI IL IN MI OH KY TN MS AL GA FL SC NC VA WV DE MD PA NY NJ CT RI MA VT NH ME DC).sort!
+  		
+  # 	@locations = Location.all
+  			
+  # 	results = HTTParty.get("http://api.wunderground.com/api/#{ENV['wunderground_api_key']}/conditions/q/#{params[:state]}/#{city_param}.json")
+  # 		if results['response']['error'].present?
+  # 			@error = results['response']['error']['description']
+
+  # 		else
+  # 			@current_observation = results['current_observation']
+  # 			latitude = @current_observation['display_location']['latitude']
+  # 			longitude = @current_observation['display_location']['longitude']
+
+  #       location_exists = false
+  #     if params[:city].present? && params[:state].present?
+  #       Location.all.each do |location|   
+  #         if location.city == params[:city] && location.state == params[:state] 
+  #           location_exists = true 
+  #         end
+  #       end
+  #     # end
+
+  #     if location_exists == false 
+  #         Location.create(
+  #           city: params[:city],
+  #           state: params[:state],
+  #           latitude:latitude,
+  #           longitude:longitude
+  #             )
+  #     end
+  # 		end
+  # 		end
+  # 	end	
+  end
+
+  # city_param = URI.encode(params[:city])
