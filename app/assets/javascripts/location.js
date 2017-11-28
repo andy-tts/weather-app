3c73733f65242084b0737057477e1390a7d79f34
Andy TTS <awhillans@techtalentsouth.com>
2017-11-28 11:42:28 -0500

    Display map for each location

---
+++ app/assets/javascripts/location.js
@@ -0,0 +1,7 @@
+function initLocationMap() { 
+	var mapOptions = {
+		center: { lat: latitude, lng: longitude},
+		zoom: 14
+	};
+	var ourMap = new google.maps.Map(document.getElementById('location-map'), mapOptions);
+}
